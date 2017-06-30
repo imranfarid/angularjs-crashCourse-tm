807e436860e53750ee6978e59b8fd3148738d087
imranfarid <imran.cyborg@gmail.com>
2017-07-01 00:18:03 +0600

    constructor *ngFor *ngIf click-event

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -6,14 +6,48 @@ import { Component } from '@angular/core';
   <h1>Hello {{name}}</h1>
   <p><strong>Email: </strong> {{email}}</p>
   <p><strong>Address: </strong> {{address.street}} {{address.city}}, {{address.state}}</p>
+  <button (click)="toggleHobbies()" >{{showHobbies ? "Hide Hobbies" : "Show Hobbies"}}</button>
+  <div *ngIf="showHobbies">
+    <h3>Hobbies</h3>
+    <ul>
+        <li *ngFor="let hobby of hobbies">
+            {{hobby}}
+        </li>
+    </ul>
+  </div>
   `,
 })
 export class UserComponent  { 
-    name = 'Tom Hardy';
-    email = 't_hardy@gmail.com',
-    address = {
-        street: '12 Main st',
-        city: 'Boston',
-        state: 'MA'
+    name: string;
+    email: string;
+    address: address;
+    hobbies: string[];
+    showHobbies: boolean;
+
+    constructor(){
+        this.name = 'Tom Hardy';
+        this.email = 't_hardy@gmail.com',
+        this.address = {
+            street: '12 Main st',
+            city: 'Boston',
+            state: 'MA'
+        }
+        this.hobbies = ['Music', 'Movies', 'Sports'];
+        this.showHobbies = false;
+    }
+
+    toggleHobbies(){
+        if(this.showHobbies == true){
+            this.showHobbies = false;
+        }else{
+            this.showHobbies = true;
+        }
+        
     } 
+}
+
+interface address{
+    street: string;
+    city: String;
+    state: string;
 }
